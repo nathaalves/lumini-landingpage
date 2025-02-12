14d8d7f0f809899c085dec8fa030173915c2eb5c
Nathã Alves Araujo <nathaalvesaraujo@gmail.com>
2025-02-12 13:25:34 -0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,26 @@
 import '@/styles/globals.css'
 import { TRPCReactProvider } from '@/trpc/react'
-import { GeistSans } from 'geist/font/sans'
 import { type Metadata } from 'next'
+import { Poppins } from 'next/font/google'
+
+import { cn } from '@/lib/cn'
 
 export const metadata: Metadata = {
   title: 'Luminisol',
   icons: [{ rel: 'icon', url: '/favicon.ico' }]
 }
 
+const poppins = Poppins({
+  weight: ['400', '500', '700'],
+  display: 'swap',
+  subsets: ['latin']
+})
+
 export default function RootLayout({
   children
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="pt-br" className={`${GeistSans.variable}`}>
+    <html lang="pt-br" className={cn(poppins.className)}>
       <body suppressHydrationWarning>
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,32 +3,64 @@ import Image from 'next/image'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger
+} from '@/components/ui/sheet'
 
 export default function HomePage() {
   return (
     <div className="w-full">
-      <div className="flex items-center justify-between p-4">
+      <div className="absolute left-0 right-0 top-0 z-10 flex items-center justify-between p-4">
         <Image
           src="/logo-icon.png"
           width={50}
           height={50}
           alt="Description of the image"
         />
-        <Menu className="h-10 w-10" />
+        <Sheet>
+          <SheetTrigger>
+            <Menu className="h-10 w-10 stroke-white" />
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle className="hidden">
+                Menu de navegação da landing page
+              </SheetTitle>
+            </SheetHeader>
+          </SheetContent>
+        </Sheet>
       </div>
-      <div className="mx-20 mt-10 flex flex-col text-center">
-        <p className="text-5xl">Economize 95%</p>
-        <p className="text-4xl">da sua conta de energia</p>
-        <p className="my-6">
-          Chegou a hora de reduzir seus gastos fixos com energia elétrica. Faça
-          uma simulação e descubra em poucos segundos o valor do seu
-          investimento.
-        </p>
-        <p className="font-semibold">
-          Qual o gasto mensal médio da sua conta de energia?
-        </p>
-        <Input className="mb-4 mt-2" placeholder="R$ 300,00" />
-        <Button className="mx-4 text-xs">FAZER UMA SIMULAÇÃO</Button>
+      <div className="relative h-svh pt-40">
+        <Image
+          src="/background-2.jpg"
+          fill={true}
+          className="-z-10 h-full w-auto object-cover"
+          alt=""
+        />
+        <div className="mx-16 flex flex-col text-center text-white">
+          <p className="text-5xl font-bold text-[#fba800]">Economize 95%</p>
+          <p className="text-4xl">da sua conta de energia</p>
+          <p className="my-6">
+            Chegou a hora de reduzir seus gastos fixos com energia elétrica.
+            Faça uma simulação e descubra em poucos segundos o valor do seu
+            investimento.
+          </p>
+          <p className="font-semibold">
+            Qual o gasto mensal médio da sua conta de energia?
+          </p>
+          <Input
+            className="mb-4 mt-2 bg-white focus-visible:ring-2 focus-visible:ring-[#fba800]"
+            placeholder="R$ 300,00"
+          />
+          <Button className="mx-4 bg-gradient-to-r from-[#fba800] to-[#fa7600] text-xs">
+            FAZER UMA SIMULAÇÃO
+          </Button>
+        </div>
       </div>
     </div>
   )
